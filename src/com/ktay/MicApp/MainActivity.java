7957d13f53b22e8d5bbb97469c429994965cb61b
ktay <munxtwo@gmail.com>
2015-04-08 13:17:56 -0700

    Added toggle button.

--- src/com/ktay/MicApp/MainActivity.java
+++ src/com/ktay/MicApp/MainActivity.java
@@ -2,6 +2,9 @@ package com.ktay.MicApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +14,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    Toast.makeText(getApplicationContext(), "enabled", Toast.LENGTH_SHORT).show();
+                } else {
+                    // The toggle is disabled
+                    Toast.makeText(getApplicationContext(), "disabled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
