5428e46745e4c652deced277ad75e3ba1ae54848
munxtwo <munxtwo@gmail.com>
2015-07-05 17:19:16 -0700

    Disabled screen orientation on ToggleMicActivity.

--- src/com/ktay/MicApp/activity/ToggleMicActivity.java
+++ src/com/ktay/MicApp/activity/ToggleMicActivity.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -34,6 +35,8 @@ public class ToggleMicActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
 
 		mBroadcastReceiver = createBroadcastReceiver();
 
